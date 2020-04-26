3adcd53731842dad1e51aea6b94ceb2c517d3cbd
Jeff Wong <v-jwong1@expedia.com>
2020-04-26 15:12:46 +1000

    initial commit

---
+++ src/test/java/com/pluralsight/conferencespringboot/ConferenceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferencespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
