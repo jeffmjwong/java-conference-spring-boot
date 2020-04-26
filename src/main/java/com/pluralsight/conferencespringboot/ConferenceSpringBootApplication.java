4d62fb3d28ea1f2297978894a89ca663699a7ca2
Jeff Wong <v-jwong1@expedia.com>
2020-04-26 15:35:30 +1000

    format whitespace

--- src/main/java/com/pluralsight/conferencespringboot/ConferenceSpringBootApplication.java
+++ src/main/java/com/pluralsight/conferencespringboot/ConferenceSpringBootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConferenceSpringBootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceSpringBootApplication.class, args);
 	}
-
 }
