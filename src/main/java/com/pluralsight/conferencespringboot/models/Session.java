e3baf993e55f2f1ba96436cecbe01e2dd5fd0a45
Jeff Wong <v-jwong1@expedia.com>
2020-05-02 15:33:14 +1000

    mark session_id with @Id annotation

--- src/main/java/com/pluralsight/conferencespringboot/models/Session.java
+++ src/main/java/com/pluralsight/conferencespringboot/models/Session.java
@@ -1,9 +1,14 @@
 package com.pluralsight.conferencespringboot.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity(name = "sessions")
 public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long session_id;
     private String session_name;
     private String session_description;
