795059ff8721263b8b40f13361d6f8dfe44889ef
Jeff Wong <v-jwong1@expedia.com>
2020-05-02 17:44:36 +1000

    specify http status of created for post request

--- src/main/java/com/pluralsight/conferencespringboot/controllers/SessionsController.java
+++ src/main/java/com/pluralsight/conferencespringboot/controllers/SessionsController.java
@@ -3,11 +3,13 @@ package com.pluralsight.conferencespringboot.controllers;
 import com.pluralsight.conferencespringboot.models.Session;
 import com.pluralsight.conferencespringboot.repositories.SessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -30,6 +32,7 @@ public class SessionsController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Session create(@RequestBody final Session session) {
         return sessionRepository.saveAndFlush(session);
     }
