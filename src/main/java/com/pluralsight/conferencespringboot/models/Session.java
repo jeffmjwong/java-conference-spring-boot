7d52a24f6b5c2fcc11c285ff5ecddb14ef0ae5f6
Jeff Wong <v-jwong1@expedia.com>
2020-05-02 15:27:19 +1000

    fields for Session object

--- src/main/java/com/pluralsight/conferencespringboot/models/Session.java
+++ src/main/java/com/pluralsight/conferencespringboot/models/Session.java
@@ -4,5 +4,10 @@ import javax.persistence.Entity;
 
 @Entity(name = "sessions")
 public class Session {
+    private Long sessionId;
+    private String sessionName;
+    private String sessionDescription;
+    private Integer sessionLength;
+
     public Session() {}
 }
