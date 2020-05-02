cf41363c30260be9c0513f948b1391342601c646
Jeff Wong <v-jwong1@expedia.com>
2020-05-02 16:14:22 +1000

    list of sessions in speaker class

--- src/main/java/com/pluralsight/conferencespringboot/models/Speaker.java
+++ src/main/java/com/pluralsight/conferencespringboot/models/Speaker.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
 
 @Entity(name = "speakers")
 public class Speaker {
@@ -15,6 +16,8 @@ public class Speaker {
     private String title;
     private String company;
     private String speaker_bio;
+
+    private List<Session> sessions;
 
     public Speaker() {}
 
@@ -64,5 +67,13 @@ public class Speaker {
 
     public void setSpeaker_bio(String speaker_bio) {
         this.speaker_bio = speaker_bio;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
     }
 }
