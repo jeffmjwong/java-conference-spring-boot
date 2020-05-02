77550526a1a319f41c10af83f9366a6aeb690a0b
Jeff Wong <v-jwong1@expedia.com>
2020-05-02 16:15:49 +1000

    mark @ManyToMany on Speaker class side

--- src/main/java/com/pluralsight/conferencespringboot/models/Speaker.java
+++ src/main/java/com/pluralsight/conferencespringboot/models/Speaker.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import java.util.List;
 
 @Entity(name = "speakers")
@@ -17,6 +18,7 @@ public class Speaker {
     private String company;
     private String speaker_bio;
 
+    @ManyToMany(mappedBy = "speakers")
     private List<Session> sessions;
 
     public Speaker() {}
