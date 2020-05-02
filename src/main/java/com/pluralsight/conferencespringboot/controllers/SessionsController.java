41c612ac25bf9382e5a84c0068d73c10a2a5aa21
Jeff Wong <v-jwong1@expedia.com>
2020-05-02 17:30:12 +1000

    autowire SessionRepository to SessionsController

--- src/main/java/com/pluralsight/conferencespringboot/controllers/SessionsController.java
+++ src/main/java/com/pluralsight/conferencespringboot/controllers/SessionsController.java
@@ -1,10 +1,13 @@
 package com.pluralsight.conferencespringboot.controllers;
 
+import com.pluralsight.conferencespringboot.repositories.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/sessions")
 public class SessionsController {
-
+    @Autowired
+    private SessionRepository sessionRepository;
 }
