c578013b3c7e7f2dac2b020bc12dcdad40126cb6
Jeff Wong <v-jwong1@expedia.com>
2020-05-03 17:04:08 +1000

    root route in HomeController to return app version

--- src/main/java/com/pluralsight/conferencespringboot/controllers/HomeController.java
+++ src/main/java/com/pluralsight/conferencespringboot/controllers/HomeController.java
@@ -1,4 +1,23 @@
 package com.pluralsight.conferencespringboot.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class HomeController {
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping
+    @RequestMapping("/")
+    public Map<String, String> getStatus() {
+        final Map<String, String> map = new HashMap<>();
+        map.put("app-version", appVersion);
+        return map;
+    }
 }
