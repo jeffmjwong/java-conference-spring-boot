961446178b996e1336bdc17abb834a2f1d9c4a3e
Jeff Wong <v-jwong1@expedia.com>
2020-05-03 17:09:05 +1000

    create dataSource method as a bean in PersistenceConfiguration

--- src/main/java/com/pluralsight/conferencespringboot/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencespringboot/config/PersistenceConfiguration.java
@@ -1,4 +1,14 @@
 package com.pluralsight.conferencespringboot.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class PersistenceConfiguration {
+    @Bean
+    public DataSource dataSource() {
+
+    }
 }
