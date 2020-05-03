ce1fdf6976c66a7b1665bb8c27b1f3dc8ad8b36a
Jeff Wong <v-jwong1@expedia.com>
2020-05-03 17:13:10 +1000

    replace default data source with custom data source bean

--- src/main/java/com/pluralsight/conferencespringboot/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencespringboot/config/PersistenceConfiguration.java
@@ -1,5 +1,6 @@
 package com.pluralsight.conferencespringboot.config;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,9 @@ import javax.sql.DataSource;
 public class PersistenceConfiguration {
     @Bean
     public DataSource dataSource() {
-
+        final DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        System.out.println("My custom data source bean has been initialized and set!");
+        return builder.build();
     }
 }
