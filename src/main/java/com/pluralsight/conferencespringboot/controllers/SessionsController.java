a7e9749cd004265cf7e8ef7c3e86207594cacc7b
Jeff Wong <v-jwong1@expedia.com>
2020-05-03 12:33:06 +1000

    PUT method for SessionsController

--- src/main/java/com/pluralsight/conferencespringboot/controllers/SessionsController.java
+++ src/main/java/com/pluralsight/conferencespringboot/controllers/SessionsController.java
@@ -2,6 +2,7 @@ package com.pluralsight.conferencespringboot.controllers;
 
 import com.pluralsight.conferencespringboot.models.Session;
 import com.pluralsight.conferencespringboot.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,5 +40,14 @@ public class SessionsController {
     public void delete(@PathVariable Long id) {
         // Also need to check for children record before deleting.
         sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        // Because this is a PUT, we expect all attributes to be passed in. A PATCH would only need what get passed in.
+        // TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
+        final Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
     }
 }
