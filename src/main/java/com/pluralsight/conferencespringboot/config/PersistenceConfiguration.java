df539bd9ea4563938a4be34457da744645b200e2
Jeff Wong <v-jwong1@expedia.com>
2020-05-03 17:18:25 +1000

    inject db url, username and password from application.properties file

--- src/main/java/com/pluralsight/conferencespringboot/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencespringboot/config/PersistenceConfiguration.java
@@ -1,5 +1,6 @@
 package com.pluralsight.conferencespringboot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +9,21 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
     @Bean
     public DataSource dataSource() {
         final DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        builder.url(dbUrl);
+        builder.username(dbUsername);
+        builder.password(dbPassword);
         System.out.println("My custom data source bean has been initialized and set!");
         return builder.build();
     }
